1618ddf6394adc880cf0d707574296c3542e7267
Pablo Dallegri <pdallegri@dc.uba.ar>
2022-06-08 05:26:00 -0300

    Ejercicio 2, borro texto para informe del .cpp

--- EJ_2.cpp
+++ EJ_2.cpp
@@ -1,56 +1,3 @@
-/*
-Ejercicio 2
-
-El grafo G(M) asociado a una matriz M ∈ {0,1}ᵐˣⁿ tiene un vértice por cada posición de M que tiene valor 1, donde dos vértices de G(M) son adyacentes si y solo si sus posiciones son adyacentes en M. Diseñar un algoritmo que, dada una matriz M, determine la cantidad de componentes conexas de G(M). El algoritmo debe consumir espacio O(n)* y tiempo O(mnα⁻¹(nm)) donde α es la función de Ackerman.
-* Sin contar el espacio que ocupa la matriz M en el disco. Solo contar lo que se consume de memoria. Notar que M no se puede cargar enteramente en memoria, sino que solo se puede leer una cantidad constante de las en simultáneo.
-
-Descripción de una instancia.
-La instancia empieza con una línea con los valores m y n que representan la cantidad de las y columnas de la matriz M. Luego sigen m líneas, cada una de las cuales tiene n valores en {0,1}. El j-ésimo valor de la i-ésima fila se corresponde a Mᵢⱼ.
-
-Descripción de la salida.
-Una línea con la cantidad de componentes conexas de G(M).
-
-Entrada de ejemplo
-4 10
-0 0 1 1 1 1 0 0 1 1
-0 1 1 0 0 1 1 1 1 1
-0 0 1 1 1 1 0 1 0 0
-1 0 0 0 0 0 0 0 1 1
-
-Salida esperada de ejemplo
-3
-*/
-
-
-/*
-Planteo de solución
-
-Se sabe que cualquier subgrafo inducido o compresión de un grafo planar también resulta planar.
-Se sabe que un grafo planar de n vertices y e aristas cumple que: e ≤ 3n.
-Se sabe que G(M) es subgrafo inducido de un grafo reticular planar.
-
-Se propone el siguiente invariante para un ciclo que procesó la matriz hasta fila i inclusive:
-Sea Rᵢ el subgrafo inducido por G(M) al quitar todos los vertices correspondientes a filas de M posteriores a la fila i,
-cada vértice de la fila i tiene asignado un identificador exclusivo a la componente conexa en Rᵢ a la que pertenece.
-Se conoce el conteo Kᵢ de componentes conexas de G(M) contenidas en Rᵢ, que incialmente vale 0.
-
-Con dicho invariante, se propone el siguiente método para procesar la fila i+1:
-Sea F el subgrafo inducido por G(M) al quitar todos los vertices no correspondientes a la fila por procesar,
-asignar a cada vértice de la fila i+1 un identificador exclusivo a la componente conexa en F a la que pertence.
-Sea I el subgrafo inducido por G(M) al quitar todos los vertices no correspondientes a las filas i e i+1, y sea C una compresión de I al 
-reemplazar cada conjunto de vértices con mismo identificador y misma fila por un único vértice, realizar un único recorrido del grafo C para 
-identificar sus componentes conexas.
-Incrementar el conteo K según la cantidad de vértices de C en la fila i que estén aislados.
-Almacenar para cada vertice de G(M) en la fila i+1, un identificador exclusivo a la componente conexa en C a la que pertence el vértice dentro del 
-cual fue comprimido, pues será de utilidad en la próxima iteración.
-
-El recorrido del grafo C es realizable en complejidad temporal y espacial de orden n, siendo n el largo de cada fila, pues se tiene que C es un 
-grafo planar con no más de 2*⌈n/2⌉ vertices, lo cual implica que como máximo alberga 3*2*⌈n/2⌉ aristas.
-Tras iterar sobre las m filas de la matriz, se logra una complejidad temporal de O(n*m)
-
-Como para efectuar cada iteración solo es necesario conocer los identificadores de las componentes conexas en Rᵢ de los vértices correspondientes a la última fila procesada, cuya cantidad no puede exceder ⌈n/2⌉, se logra una complejidad espacial O(n) para todo el algoritmo, sin icluir el espacio donde inicie almacenada la matriz de entrada M.
-*/
-
 #include <iostream>
 #include <cassert>
 #include <vector>
@@ -172,4 +119,3 @@ int main() {
 	// Imprimir resultado
 	cout << componentes_cerradas << endl;
 }
-
